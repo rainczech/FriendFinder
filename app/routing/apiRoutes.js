6d5bbd60f93fbf040ea87fd00cf3358fe31504e5
Grace Ho <rainczech@gmail.com>
2018-05-03 09:46:40 -0500

    apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,6 +2,20 @@ app.get("/api/friends", function(req, res){
     res.sendFile(path.join(__dirname, "home.html"));
 });
 
+app.post("/api/friends", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body-parser middleware
+    var newFriend = req.body;
+  
+    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newcharacter);
+  
+    friends.push(newFriend);
+  
+    res.json(newcharacter);
+  });
+  
 
 
 
